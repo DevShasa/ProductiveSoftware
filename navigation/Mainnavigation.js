914167a68e02ba77f5c9a4db0a4617c4746cc454
DevShasa <shasa.thuo@gmail.com>
2024-01-19 22:01:23 +0300

    scaffolding for the app

---
+++ navigation/Mainnavigation.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Mainnavigation = () => {
+  return (
+    <View>
+      <Text>Mainnavigation</Text>
+    </View>
+  )
+}
+
+export default Mainnavigation
+
+const styles = StyleSheet.create({})
---
+++ screens/Userprofile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Userprofile = () => {
+  return (
+    <View>
+      <Text>Userprofile</Text>
+    </View>
+  )
+}
+
+export default Userprofile
+
+const styles = StyleSheet.create({})
