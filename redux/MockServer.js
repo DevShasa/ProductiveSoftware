002c9f5a871add23f405353f70edfca7379f1d3d
DevShasa <shasa.thuo@gmail.com>
2024-01-19 22:28:03 +0300

    Mirage

---
+++ redux/MockServer.js
@@ -0,0 +1,2 @@
+// this is where we define mirage stuff https://miragejs.com/quickstarts/react-native/development/
+// https://github.com/KaungHtetHein116/Travel-Blog-React-Native/blob/master/mockServer/factories/destination.js
